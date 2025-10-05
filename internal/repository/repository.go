f4161226dfe192c7c29e293160b40205501d2b1d
murlock <vanay2015ooo@yandex.ru>
2025-10-05 23:53:53 +0300

    Repository aggregator

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,19 @@
 package repository
+
+import (
+	"courier-bot/internal/repository/interfaces"
+	"courier-bot/internal/repository/postgres"
+	"database/sql"
+)
+
+type Repository struct {
+	Courier         interfaces.CourierRepository
+	OrderAssignment interfaces.OrderAssignment
+}
+
+func NewRepository(db *sql.DB) *Repository {
+	return &Repository{
+		Courier:         postgres.NewCourierRepository(db),
+		OrderAssignment: postgres.NewOrderAssignmentRepository(db),
+	}
+}
