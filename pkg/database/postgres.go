50aa9079b200af00aeb5acdce06d403ce61fc86f
murlock <vanay2015ooo@yandex.ru>
2025-10-18 21:54:50 +0300

    Postgres Connection Creation

---
+++ pkg/database/postgres.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"courier-bot/internal/config"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func NewPostgresDB(cfg *config.Config) (*sql.DB, error) {
+	conStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName,
+	)
+
+	db, err := sql.Open("postgres", conStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetConnMaxIdleTime(2 * time.Minute)
+
+	return db, nil
+}
